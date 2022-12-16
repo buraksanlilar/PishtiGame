deb0770c4486b8806b99934c3760c68d1aa51c94
buraksanlilar <120301435+buraksanlilar@users.noreply.github.com>
2022-12-16 21:56:55 +0300

    cutdeck function
    cut function finally finished

--- Deck.java
+++ Deck.java
@@ -1,26 +1,43 @@
-import java.util.Random;
 public class Deck {
-    Random rd = new Random();
     private String [] deck = new String [52];
     private String [] values = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
     private String [] suits = {"S","C","H","D"};
-    public void getDeck () {
+    public void setDeck () {
     for(int i=0;i<deck.length;i++) {
         deck [i] =values[i%13] + suits[i/13];
-        System.out.println(deck[i]);
+        
     }
    
+}
+public void getDeck() {
+    for(int i = 0; i<deck.length;i++) {
+        System.out.println(deck[i]);
+    }
 }
 public void shuffle() {
 for (int i =0 ; i<deck.length;i++) {
     String temp = deck[i];
-    int random= (int)rd.nextInt(deck.length);
+    int random= (int)(Math.random()*deck.length);
     deck[i]= deck[random];
     deck[random]=temp;
-    System.out.println(deck[i]);
 }
-
 }
-  
-   
+public void cut(int c) {
+String [] top = new String[deck.length-c];
+String [] bot = new String[c];
+for (int i = 0; i < c; i++) {
+    bot[i] = deck[i];
+  }
+  for (int i = 0; i < top.length; i++) {
+    top[i] = deck[c + i];
+  }
+ String[] newDeck = new String[deck.length];
+ for (int i = 0; i<top.length;i++) {
+newDeck[i]=top[i];
+ }
+ for (int i = 0 ; i<bot.length;i++) {
+newDeck[top.length+i]=bot[i];
+ }
+ deck = newDeck;
+  }
 }
--- Pishti.java
+++ Pishti.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 public class Pishti {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     Deck deck1 = new Deck();
+    deck1.setDeck();
+    deck1.shuffle();
+    System.out.println("before");
     deck1.getDeck();
-    
-}
+    System.out.println("after the cut");
+    deck1.cut(1);
+    deck1.getDeck();
+   
+
+
+    }
 }
